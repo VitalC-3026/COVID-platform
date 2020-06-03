436b972eff84d6fad55029d7ebdc483e87328717
fengjk <1327246148@qq.com>
2020-06-03 16:14:47 +0800

    正确version

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-<?php
-namespace frontend\models;
-
-use common\models\MyUser;
-use Yii;
-use yii\base\Model;
-
-/**
- * Signup form
- */
-class SignupForm extends Model
-{
-    public $account;
-    public $password;
-    public $username;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            ['account', 'trim'],
-            ['account', 'required'],
-            ['account', 'unique', 'targetClass' => '\common\models\MyUser', 'message' => 'This account has already been taken.'],
-            ['account', 'string', 'length' => 18],
-
-            ['username', 'trim'],
-            ['username', 'required'],
-            ['username', 'string', 'min' => 1, 'max' => 20],
-
-            ['password', 'required'],
-            ['password', 'string', 'min' => 4],
-        ];
-    }
-
-    /**
-     * Signs user up.
-     *
-     * @return bool whether the creating new account was successful and email was sent
-     */
-    public function signup()
-    {
-        if (!$this->validate()) {
-            return null;
-        }
-        
-        $user = new MyUser();
-        $user->account = $this->account;
-        $user->setPassword($this->password);
-        $user->setUsername($this->username);
-        $user->generateAuthKey();
-        return $user->save() ;
-    }
-}
-=======
 <?php
 namespace frontend\models;
 
@@ -110,4 +53,3 @@ class SignupForm extends Model
         return $user->save() ;
     }
 }
->>>>>>> 49b5bc7fdaf65392809418497d4db7a51fc8a54e
