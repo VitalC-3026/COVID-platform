d32e0f36e4b5bd6cf5e1c9648c7a918256d711e8
fengjk <1327246148@qq.com>
2020-06-08 21:36:51 +0800

    后台改由路由跳转，后台logo更换

--- frontend/modules/backend/views/layouts/main.php
+++ frontend/modules/backend/views/layouts/main.php
@@ -1,5 +1,6 @@
 <?php
 /* @var $content string */
+
 use frontend\assets\AppAsset_b;
 use yii\helpers\Html;
 use yii\heplers\Url;
@@ -11,62 +12,66 @@ use common\widgets\Alert;
 AppAsset_b::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-<!--<![endif]-->
+    <!DOCTYPE html>
+    <!--[if lt IE 7]>
+    <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+    <!--[if IE 7]>
+    <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+    <!--[if IE 8]>
+    <html class="no-js lt-ie9"> <![endif]-->
+    <!--[if gt IE 8]><!-->
+    <html class="no-js">
+    <!--<![endif]-->
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>COVID-Platform</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <!-- Favicon -->
-    <!-- <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon"> -->
-    <link rel="shortcut icon" href="assets/img/favicon.png">
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-    <!-- Fonts from Font Awsome -->
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <!-- CSS Animate -->
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <!-- Custom styles for this theme -->
-    <link rel="stylesheet" href="assets/css/main.css">
-    <!-- Vector Map  -->
-    <link rel="stylesheet" href="assets/plugins/jvectormap/css/jquery-jvectormap-1.2.2.css">
-    <!-- ToDos  -->
-    <link rel="stylesheet" href="assets/plugins/todo/css/todos.css">
-    <!-- Morris  -->
-    <link rel="stylesheet" href="assets/plugins/morris/css/morris.css">
-    <!-- Fonts -->
-    <!-- <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'> -->
-    <!-- Feature detection -->
-    <script src="assets/js/modernizr-2.6.2.min.js"></script>
-    <script src="https://cdn.ckeditor.com/ckeditor5/19.1.1/classic/ckeditor.js"></script>
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="assets/js/html5shiv.js"></script>
-    <script src="assets/js/respond.min.js"></script>
-    <![endif]-->
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>COVID-Platform</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <!-- Favicon -->
+        <!-- <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon"> -->
+        <link rel="shortcut icon" href="assets/img/favicon.png">
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+        <!-- Fonts from Font Awsome -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <!-- CSS Animate -->
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <!-- Custom styles for this theme -->
+        <link rel="stylesheet" href="assets/css/main.css">
+        <!-- Vector Map  -->
+        <link rel="stylesheet" href="assets/plugins/jvectormap/css/jquery-jvectormap-1.2.2.css">
+        <!-- ToDos  -->
+        <link rel="stylesheet" href="assets/plugins/todo/css/todos.css">
+        <!-- Morris  -->
+        <link rel="stylesheet" href="assets/plugins/morris/css/morris.css">
+        <!-- Fonts -->
+        <!-- <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'> -->
+        <!-- Feature detection -->
+        <script src="assets/js/modernizr-2.6.2.min.js"></script>
+        <script src="https://cdn.ckeditor.com/ckeditor5/19.1.1/classic/ckeditor.js"></script>
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="assets/js/html5shiv.js"></script>
+        <script src="assets/js/respond.min.js"></script>
+        <![endif]-->
+    </head>
 
-<body>
-<?php $this->beginBody() ?>
+    <body>
+    <?php $this->beginBody() ?>
     <section id="container">
         <header id="header">
             <!--logo start-->
             <div class="brand" style="background-color: #2fcbce">
-                <a href="index.html" class="logo">
-                    <span>Space</span>Lab</a>
+                <a href="<?php echo Yii::$app->getHomeUrl(); ?>" class="logo">
+                    <span>平安</span>社区</a>
             </div>
             <!--logo end-->
             <div class="toggle-navigation toggle-left">
-                <button type="button" class="btn btn-default" id="toggle-left" data-toggle="tooltip" data-placement="right" title="Toggle Navigation">
+                <button type="button" class="btn btn-default" id="toggle-left" data-toggle="tooltip"
+                        data-placement="right" title="Toggle Navigation">
                     <i class="fa fa-bars"></i>
                 </button>
             </div>
@@ -74,7 +79,8 @@ AppAsset_b::register($this);
                 <ul>
                     <li class="dropdown messages">
                         <span class="badge badge-danager animated bounceIn" id="new-messages">5</span>
-                        <button type="button" class="btn btn-default dropdown-toggle options" id="toggle-mail" data-toggle="dropdown">
+                        <button type="button" class="btn btn-default dropdown-toggle options" id="toggle-mail"
+                                data-toggle="dropdown">
                             <i class="fa fa-envelope"></i>
                         </button>
                         <ul class="dropdown-menu alert animated fadeInDown">
@@ -90,7 +96,9 @@ AppAsset_b::register($this);
                                     <div class="message-info">
                                         <span class="sender">James Bagian</span>
                                         <span class="time">30 mins</span>
-                                        <div class="message-content">Lorem ipsum dolor sit amet, elit rutrum felis sed erat augue fusce...</div>
+                                        <div class="message-content">Lorem ipsum dolor sit amet, elit rutrum felis sed
+                                            erat augue fusce...
+                                        </div>
                                     </div>
                                 </a>
                             </li>
@@ -103,7 +111,9 @@ AppAsset_b::register($this);
                                     <div class="message-info">
                                         <span class="sender">Jeffrey Ashby</span>
                                         <span class="time">2 hour</span>
-                                        <div class="message-content">hendrerit pellentesque, iure tincidunt, faucibus vitae dolor aliquam...</div>
+                                        <div class="message-content">hendrerit pellentesque, iure tincidunt, faucibus
+                                            vitae dolor aliquam...
+                                        </div>
                                     </div>
                                 </a>
                             </li>
@@ -116,7 +126,9 @@ AppAsset_b::register($this);
                                     <div class="message-info">
                                         <span class="sender">John Douey</span>
                                         <span class="time">3 hours</span>
-                                        <div class="message-content">Penatibus suspendisse sit pellentesque eu accumsan condimentum nec...</div>
+                                        <div class="message-content">Penatibus suspendisse sit pellentesque eu accumsan
+                                            condimentum nec...
+                                        </div>
                                     </div>
                                 </a>
                             </li>
@@ -129,7 +141,9 @@ AppAsset_b::register($this);
                                     <div class="message-info">
                                         <span class="sender">Ellen Baker</span>
                                         <span class="time">7 hours</span>
-                                        <div class="message-content">Sem dapibus in, orci bibendum faucibus tellus, justo arcu...</div>
+                                        <div class="message-content">Sem dapibus in, orci bibendum faucibus tellus,
+                                            justo arcu...
+                                        </div>
                                     </div>
                                 </a>
                             </li>
@@ -141,7 +155,9 @@ AppAsset_b::register($this);
                                     <div class="message-info">
                                         <span class="sender">Ivan Bella</span>
                                         <span class="time">6 hours</span>
-                                        <div class="message-content">Curabitur metus faucibus sapien elit, ante molestie sapien...</div>
+                                        <div class="message-content">Curabitur metus faucibus sapien elit, ante molestie
+                                            sapien...
+                                        </div>
                                     </div>
                                 </a>
                             </li>
@@ -155,8 +171,8 @@ AppAsset_b::register($this);
                     </li>
                     <li class="dropdown settings">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                      Mike Adams <i class="fa fa-angle-down"></i>
-                    </a>
+                            Mike Adams <i class="fa fa-angle-down"></i>
+                        </a>
                         <ul class="dropdown-menu animated fadeInDown">
                             <li>
                                 <a href="#"><i class="fa fa-user"></i> Profile</a>
@@ -165,7 +181,8 @@ AppAsset_b::register($this);
                                 <a href="#"><i class="fa fa-calendar"></i> Calendar</a>
                             </li>
                             <li>
-                                <a href="#"><i class="fa fa-envelope"></i> Inbox <span class="badge badge-danager" id="user-inbox">5</span></a>
+                                <a href="#"><i class="fa fa-envelope"></i> Inbox <span class="badge badge-danager"
+                                                                                       id="user-inbox">5</span></a>
                             </li>
                             <li>
                                 <a href="#"><i class="fa fa-power-off"></i> Logout</a>
@@ -183,37 +200,57 @@ AppAsset_b::register($this);
                 </ul>
             </div>
         </header>
+
+
         <!--sidebar left start-->
         <aside class="sidebar">
             <div id="leftside-navigation" class="nano">
                 <ul class="nano-content">
                     <?php //TODO: 路由跳转?>
                     <li class="active">
-                        <a href="http://localhost:8080/yii/COVID-platform/frontend/web/index.php?r=backend%2Fsite%2Findex"><i class="fa fa-dashboard"></i><span>Dashboard</span></a>
+                        <a href="<?= \yii\helpers\Url::to(['/backend/site/index']); ?>"><i class="fa fa-dashboard"></i><span>Dashboard</span></a>
                     </li>
+
                     <li class="active">
-                        <a href="javascript:void(0);"><i class="fa fa-cogs"></i><span>社区数据库</span><i class="arrow fa fa-angle-right pull-right"></i></a>
+                        <a href="javascript:void(0);"><i class="fa fa-cogs"></i><span>社区数据库</span><i
+                                    class="arrow fa fa-angle-right pull-right"></i></a>
                         <ul>
-                        	<li><a href="http://localhost:8080/yii/COVID-platform/frontend/web/index.php?r=backend%2Fsite%2Fresinfo">居民数据库</a></li>
-                        	<li><a href="http://localhost:8080/yii/COVID-platform/frontend/web/index.php?r=backend%2Fsite%2Fadmininfo">职员数据库</a></li>
-                            <li><a href="http://localhost:8080/yii/COVID-platform/frontend/web/index.php?r=backend%2Fsite%2Frights">职员权限分配</a></li>
+                            <li>
+                                <a href="<?= \yii\helpers\Url::to(['/backend/site/resinfo']); ?>">居民数据库</a>
+                            </li>
+                            <li>
+                                <a href="<?= \yii\helpers\Url::to(['/backend/site/admininfo']); ?>">职员数据库</a>
+                            </li>
+                            <li>
+                                <a href="<?= \yii\helpers\Url::to(['/backend/site/rights']); ?>">职员权限分配</a>
+                            </li>
                         </ul>
                     </li>
                     <li class="active">
-                        <a href="javascript:void(0);"><i class="fa fa-cogs"></i><span>信息发布</span><i class="arrow fa fa-angle-right pull-right"></i></a>
+                        <a href="javascript:void(0);"><i class="fa fa-cogs"></i><span>信息发布</span><i
+                                    class="arrow fa fa-angle-right pull-right"></i></a>
                         <ul>
-                            <li><a href="http://localhost:8080/yii/COVID-platform/frontend/web/index.php?r=backend%2Fsite%2Fedit">信息编辑</a></li>
-                            <li><a href="http://localhost:8080/yii/COVID-platform/frontend/web/index.php?r=backend%2Fsite%2Fcensor">信息审核</a></li>
+                            <li>
+                                <a href="<?= \yii\helpers\Url::to(['/backend/site/edit']); ?>">信息编辑</a>
+                            </li>
+                            <li>
+                                <a href="<?= \yii\helpers\Url::to(['/backend/site/censor']); ?>">信息审核</a>
+                            </li>
                         </ul>
                     </li>
                     <li class="active">
-                        <a href="javascript:void(0);"><i class="fa fa-cogs"></i><span>同心协力战疫情</span><i class="arrow fa fa-angle-right pull-right"></i></a>
+                        <a href="javascript:void(0);"><i class="fa fa-cogs"></i><span>同心协力战疫情</span><i
+                                    class="arrow fa fa-angle-right pull-right"></i></a>
                         <ul>
-                            <li><a href="http://localhost:8080/yii/COVID-platform/frontend/web/index.php?r=backend%2Fsite%2Fhealthreport">健康日报</a></li>
-                            <li><a href="http://localhost:8080/yii/COVID-platform/frontend/web/index.php?r=backend%2Fsite%2Frequestlist">求助信息</a></li>
+                            <li>
+                                <a href="<?= \yii\helpers\Url::to(['/backend/site/healthreport']); ?>">健康日报</a>
+                            </li>
+                            <li>
+                                <a href="<?= \yii\helpers\Url::to(['/backend/site/requestlist']); ?>">求助信息</a>
+                            </li>
                         </ul>
                     </li>
-                
+
                 </ul>
             </div>
 
@@ -221,7 +258,7 @@ AppAsset_b::register($this);
         <!--sidebar left end-->
         <!--main content start-->
         <section class="main-content-wrapper">
-        	<?= $content ?>
+            <?= $content ?>
         </section>
         <!--main content end-->
         <!--sidebar right start-->
@@ -345,15 +382,15 @@ AppAsset_b::register($this);
     <script src="assets/plugins/todo/js/todos.js"></script>
     <!--Load these page level functions-->
     <script>
-    $(document).ready(function() {
-        app.timer();
-        app.map();
-        app.weather();
-        app.morrisPie();
-    });
-    </script>   
-<?php $this->endBody() ?>
-</body>
+        $(document).ready(function () {
+            app.timer();
+            app.map();
+            app.weather();
+            app.morrisPie();
+        });
+    </script>
+    <?php $this->endBody() ?>
+    </body>
 
-</html>
+    </html>
 <?php $this->endPage() ?>
