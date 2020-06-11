e4eaeb3b223bb7669eb286455c10cb96596451a5
Pixie-King <1811444@mail.nankai.edu.cn>
2020-06-12 01:22:58 +0800

    世界疫情地图

--- frontend/assets/AppAsset_b.php
+++ frontend/assets/AppAsset_b.php
@@ -39,7 +39,8 @@ class AppAsset_b extends AssetBundle
 //        'assets/plugins/waypoints/waypoints.min.js',
 //        'assets/js/application.js'
         'assets/js/echarts.min.js',
-        'assets/js/china.js'
+        'assets/js/china.js',
+        'assets/js/world.js',
 
     ];
     public $depends = [
