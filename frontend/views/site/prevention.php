a6bfa3509a21d1435c87ad076660588745e4daba
wenjingjing <1811507@mail.nankai.edu.cn>
2020-06-13 21:27:13 +0800

    页面汉化

--- frontend/views/site/prevention.php
+++ frontend/views/site/prevention.php
@@ -68,7 +68,8 @@
             <div class="col-lg-7">
                 <h1 class="mb-0 heading text-white">科学预防 保护自我</h1>
                 <br>
-                <p class="mb-0 text-white">是故圣人不治已病治未病，不治已乱治未乱，此之谓也。夫病已成而后药之，乱已成而后治之，譬犹渴而穿井，斗而铸锥，不亦晚乎？</p>
+                <p class="mb-0 text-white">是故圣人不治已病治未病，不治已乱治未乱，此之谓也。</p>
+                <p class="mb-0 text-white"> 夫病已成而后药之，乱已成而后治之，譬犹渴而穿井，斗而铸锥，不亦晚乎？</p>
             </div>
         </div>
     </div>
@@ -182,34 +183,39 @@
                                         口罩的分类有很多种包括纱布口罩、医用口罩、防尘口罩、防油口罩、N95口罩等。但是只有医用口罩和n95口罩预防新冠肺炎。医用口罩的面体部分可分为内中外三层，内层为亲肤材质，中层为隔离层，外层为特殊材料抑菌层，医用口罩能够阻隔微小的病毒气溶胶。N95口罩能够阻隔直径0.075微米的微粒，且成功率能够达到95%以上。</p>
                                     <p class="mb-4">
                                         保证口罩完全罩住口鼻。调整口罩位置，正确覆盖范围是下巴至眼睛下方1CM。摘口罩的时候，尽量不要触摸口罩的外侧，摘下口罩后记得立即洗手。一次性医用口罩不要重复使用。一旦口罩破了或者脏了，需要立即更换新的口罩。</p>
-                                    <p><a href="https://news.dayoo.com/gzrbyc/202001/21/158752_53042123.htm" class="more">了解更多</a></p>
+                                    <p><a href="https://news.dayoo.com/gzrbyc/202001/21/158752_53042123.htm"
+                                          class="more">了解更多</a></p>
                                 </div>
                                 <!-- END: .item -->
 
                                 <!-- BEGIN: .item -->
                                 <div class="item">
                                     <h4 class="text-primary mb-20 subsection-heading font-weight-bold">避免密切接触</h4>
-                                    <p>尽量避免与患者或者疑似接触者近距离接触；尽量避免在未加防护情况下与养殖或野生动物近距离接触；尽量避免与生病的动物和变质的肉接触；尽量避免与生鲜市场里的流浪动物、垃圾废水接触</p>
+                                    <p>
+                                        尽量避免与患者或者疑似接触者近距离接触；尽量避免在未加防护情况下与养殖或野生动物近距离接触；尽量避免与生病的动物和变质的肉接触；尽量避免与生鲜市场里的流浪动物、垃圾废水接触</p>
                                     <p class="mb-4">与发病病例（疑似和确诊病例）有如下接触情形之一者属于密切接触者：
                                         （1）与病例共同居住、学习、工作或其他有密切接触的人员；
                                         （2）诊疗、护理、探视病例时未采取有效防护措施的医护人员、家属或其他与病例有类似近距离接触的人员；
                                         （3）病例同病室的其他患者及其陪护人员；
                                         （4）与病例乘坐同一交通工具并有近距离接触人员；
                                         （5）现场调查人员调查经评估认为符合条件的人员。</p>
-                                    <p><a href="https://k.sina.com.cn/article_5148729601_132e3610101900tzcn.html?from=movie" class="more">了解更多</a></p>
+                                    <p>
+                                        <a href="https://k.sina.com.cn/article_5148729601_132e3610101900tzcn.html?from=movie"
+                                           class="more">了解更多</a></p>
                                 </div>
                                 <!-- END: .item -->
 
                                 <!-- BEGIN: .item -->
                                 <div class="item">
                                     <h4 class="text-primary mb-20 subsection-heading font-weight-bold">避免人群密集场所</h4>
-                                        <p>1.尽量避免去人群聚集的公共场所,以减少与患病人群接触的机会。2.如必须前往公共场所,要佩戴口罩以降低接触病原体的风险。
-                                            3.注意门把手导致的交叉感染,尽量用手肘或者肩膀去开关门,戴手套拧门把手,或者在接触后立即洗手。4.尽量选择通风良好的场所。
-                                            5.尽量避免去疾病流行地区以降低感染风险。</p>
-                                    <p class="mb-4">疫情严重时，商场中的酒吧、舞厅、电影院、电子游戏厅等人员密集的娱乐区域应当关闭；商场中的学习培训机构应当暂停组织集中学习培训，建议使用网络远程授课方式；
+                                    <p>1.尽量避免去人群聚集的公共场所,以减少与患病人群接触的机会。2.注意门把手导致的交叉感染,尽量用手肘或者肩膀去开关门,戴手套拧门把手,或者在接触后立即洗手。
+                                        3.尽量选择通风良好的场所。 4.尽量避免去疾病流行地区以降低感染风险。</p>
+                                    <p class="mb-4">
+                                        疫情严重时，商场中的酒吧、舞厅、电影院、电子游戏厅等人员密集的娱乐区域应当关闭；商场中的学习培训机构应当暂停组织集中学习培训，建议使用网络远程授课方式；
                                         商场中的服装专卖店等物品销售区，销售人员与顾客交谈时保持1米及以上距离，尽量不要直接接触；视情况适当控制销售大厅的顾客人员数量；商场中的运动健身区，建议顾客适当缩减健身时长；顾客健身时应当相对分散；
                                         商场中的餐饮集中区，应当推荐顾客采用打包带走方式用餐；或提供远程网络订餐，顾客取餐时注意不与服务人员直接接触。</p>
-                                    <p><a href="https://haokan.baidu.com/v?pd=wisenatural&vid=17256135487491091071" class="more">了解更多</a></p>
+                                    <p><a href="https://haokan.baidu.com/v?pd=wisenatural&vid=17256135487491091071"
+                                          class="more">了解更多</a></p>
                                 </div>
                                 <!-- END: .item -->
 
@@ -217,10 +223,11 @@
                                 <div class="item">
                                     <h4 class="text-primary mb-20 subsection-heading font-weight-bold">宅在家中</h4>
                                     <p>呆在家中，拒绝探访。勤洗手，咳嗽或者打喷嚏时要捂住口鼻，捂住口鼻的材料可直接丢弃或者正确清洗。
-                                    家属应尽量减少与密切接触者及其用品接触。如避免共用牙刷、香烟、餐具、毛巾、床单等。</p>
-                                    <p class="mb-4">可使用含氯消毒剂和过氧乙酸消毒剂，每天频繁清洁、消毒家庭成员经常碰触的物品，如床头柜、床架以及其他卧室家具。至少每天清洁、消毒浴室和厕所表面一次。
-                                    使用普通洗衣皂和清水清洗衣物、床单、浴巾、毛巾等，或者用洗衣机以60-90摄氏度和普通家用洗衣液清洗，然后完全干燥上述物品。
-                                    若出现可疑症状，比如发热、咳嗽、咽痛、胸闷、呼吸困难、乏力、精神稍差、恶心呕吐、腹泻、心慌、结膜炎、轻度四肢或腰背部肌肉酸痛等，应立即就医。</p>
+                                        家属应尽量减少与密切接触者及其用品接触。如避免共用牙刷、香烟、餐具、毛巾、床单等。</p>
+                                    <p class="mb-4">
+                                        可使用含氯消毒剂和过氧乙酸消毒剂，每天频繁清洁、消毒家庭成员经常碰触的物品，如床头柜、床架以及其他卧室家具。至少每天清洁、消毒浴室和厕所表面一次。
+                                        使用普通洗衣皂和清水清洗衣物、床单、浴巾、毛巾等，或者用洗衣机以60-90摄氏度和普通家用洗衣液清洗，然后完全干燥上述物品。
+                                        若出现可疑症状，比如发热、咳嗽、咽痛、胸闷、呼吸困难、乏力、精神稍差、恶心呕吐、腹泻、心慌、结膜炎、轻度四肢或腰背部肌肉酸痛等，应立即就医。</p>
                                     <p><a href="https://www.sohu.com/a/369278519_100008238" class="more">了解更多</a></p>
                                 </div>
                                 <!-- END: .item -->
@@ -308,9 +315,8 @@
               <img src="assets/frontend/images/flaticon/svg/001-virus.svg" alt="Virus">
             </span>
                         <span class="heading d-block">自我防护</span>
-                        <span class="excerpt">Separated they live in bookmarksgrove a large language ocean.</span>
+                        <span class="excerpt">冠状病毒传染快,宅在家中良心态。开窗通风勤诜手,外出定要口罩戴。不聚亲友不串门,人群密集应避开,万众一心战疫情,自我防控贡献在。</span>
 
-                        <span class="more">更多</span>
                     </a>
                 </div>
                 <div class="col">
@@ -318,19 +324,9 @@
             <span class="d-block img-wrap">
               <img src="assets/frontend/images/flaticon/svg/035-airplane.svg" alt="Virus">
             </span>
-                        <span class="heading d-block">Travel Advice</span>
-                        <span class="excerpt">Separated they live in bookmarksgrove a large language ocean.</span>
-                        <span class="more">More</span>
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="#" class="feature-item-29103 h-100">
-            <span class="d-block img-wrap">
-              <img src="assets/frontend/images/flaticon/svg/034-medical-mask.svg" alt="Virus">
-            </span>
-                        <span class="heading d-block">Technical Guidance</span>
-                        <span class="excerpt">Separated they live in bookmarksgrove a large language ocean.</span>
-                        <span class="more">More</span>
+                        <span class="heading d-block">旅行防护</span>
+                        <span class="excerpt">在发烧或咳嗽期间避免旅行，如在旅行途中生病，应立即通知相应负责人员，快速就医检测。</span>
+
                     </a>
                 </div>
                 <div class="col">
@@ -338,9 +334,9 @@
             <span class="d-block img-wrap">
               <img src="assets/frontend/images/flaticon/svg/012-temperature.svg" alt="Virus">
             </span>
-                        <span class="heading d-block">Community Advice</span>
-                        <span class="excerpt">Separated they live in bookmarksgrove a large language ocean.</span>
-                        <span class="more">More</span>
+                        <span class="heading d-block">社区防护</span>
+                        <span class="excerpt">测温登记，环境消毒，上门排查，外防输入,内防扩散，阻断病毒传播渠道、遏制疫情蔓延。</span>
+
                     </a>
                 </div>
                 <div class="col">
@@ -348,9 +344,9 @@
             <span class="d-block img-wrap">
               <img src="assets/frontend/images/flaticon/svg/025-epidemic-1.svg" alt="Virus">
             </span>
-                        <span class="heading d-block">Companies Advice</span>
-                        <span class="excerpt">Separated they live in bookmarksgrove a large language ocean.</span>
-                        <span class="more">More</span>
+                        <span class="heading d-block">公司防护</span>
+                        <span class="excerpt">做好复工前准备,落实防疫消杀措施。严格执行测温合格后进入的管理措施,所有人员必须进行测温。</span>
+
                     </a>
                 </div>
             </div>
@@ -366,11 +362,10 @@
         <div class="container">
             <div class="row justify-content-between">
                 <div class="col-md-5">
-                    <h2 class="section-heading line-primary">Community Preventive Measures</h2>
+                    <h2 class="section-heading line-primary">社区防护措施</h2>
                 </div>
                 <div class="col-md-6">
-                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It
-                        is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                    <p>社区是疫情联防联控、群防群控的关键防线，要推动防控资源和力量下沉，把社区这道防线守严守牢。社区防控工作事关国家抗“疫”大局，事关人民切身利益。</p>
                 </div>
             </div>
             <div class="row">
@@ -380,24 +375,31 @@
                       Add '.widget-29182-v2' class for v2 style
                     -->
                     <div class="widget-29182 widget-29182-v2 mb-0 pb-0">
-
+                        <div class="widget-inner">
+                            <h3>发放防疫物品</h3>
+                        </div>
                         <div class="widget-inner"><img src="assets/frontend/images/img_sm_1.jpg" alt="Image"
                                                        class="img-mb-2 img-fluid">
                         </div>
 
                         <div class="widget-inner">
-                            <h3>Community Guidelines</h3>
-                        </div>
-
-
-                        <div class="widget-inner">
                             <ul class="list-unstyled">
-                                <li><a href="#">Executive Order from the Mayor</a></li>
-                                <li><a href="#">Business &amp; Industry Advice</a></li>
-                                <li><a href="#">Home Quarantine</a></li>
-                                <li><a href="#">Community Food Supply</a></li>
-                                <li><a href="#">Community Lockdown</a></li>
-                                <li><a href="#">Skeletal Work Force Arrangement</a></li>
+                                <li>
+                                    <a href="https://www.sohu.com/a/369773520_120058454">
+                                        电子体温计</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.sohu.com/a/369773520_120058454">
+                                        消毒液</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.sohu.com/a/369773520_120058454">
+                                        一次性口罩</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.sohu.com/a/369773520_120058454">
+                                        电动喷雾机</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -409,22 +411,30 @@
                       Add '.widget-29182-v2' class for v2 style
                     -->
                     <div class="widget-29182 widget-29182-v2 mb-0 pb-0">
-
+                        <div class="widget-inner">
+                            <h3>采买生活用品</h3>
+                        </div>
                         <div class="widget-inner"><img src="assets/frontend/images/img_sm_2.jpg" alt="Image"
                                                        class="img-mb-2 img-fluid">
                         </div>
                         <div class="widget-inner">
-                            <h3>City Guidelines</h3>
-                        </div>
-
-                        <div class="widget-inner">
                             <ul class="list-unstyled">
-                                <li><a href="#">Executive Order from the Mayor</a></li>
-                                <li><a href="#">Business &amp; Industry Advice</a></li>
-                                <li><a href="#">Home Quarantine</a></li>
-                                <li><a href="#">Community Food Supply</a></li>
-                                <li><a href="#">Community Lockdown</a></li>
-                                <li><a href="#">Skeletal Work Force Arrangement</a></li>
+                                <li>
+                                    <a href="http://k.sina.com.cn/article_1893892941_70e2834d02000og34.html?from=society">
+                                        蔬菜粮油</a>
+                                </li>
+                                <li>
+                                    <a href="http://k.sina.com.cn/article_1893892941_70e2834d02000og34.html?from=society">
+                                        水果</a>
+                                </li>
+                                <li>
+                                    <a href="http://k.sina.com.cn/article_1893892941_70e2834d02000og34.html?from=society">
+                                        饮用水</a>
+                                </li>
+                                <li>
+                                    <a href="http://k.sina.com.cn/article_1893892941_70e2834d02000og34.html?from=society">
+                                        液化气</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -436,21 +446,29 @@
                       Add '.widget-29182-v2' class for v2 style
                     -->
                     <div class="widget-29182 widget-29182-v2 mb-0 pb-0">
+                        <div class="widget-inner">
+                            <h3>隔离措施</h3>
+                        </div>
                         <div class="widget-inner"><img src="assets/frontend/images/img_sm_3.jpg" alt="Image"
                                                        class="img-mb-2 img-fluid">
                         </div>
                         <div class="widget-inner">
-                            <h3>Lockdown Guidelines</h3>
-                        </div>
-
-                        <div class="widget-inner">
                             <ul class="list-unstyled">
-                                <li><a href="#">Executive Order from the Mayor</a></li>
-                                <li><a href="#">Business &amp; Industry Advice</a></li>
-                                <li><a href="#">Home Quarantine</a></li>
-                                <li><a href="#">Community Food Supply</a></li>
-                                <li><a href="#">Community Lockdown</a></li>
-                                <li><a href="#">Skeletal Work Force Arrangement</a></li>
+                                <li>
+                                    <a href="http://gdgz.wenming.cn/gzjj/202002/t20200207_6274584.htm">
+                                        上门随访，健康检测
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://gdgz.wenming.cn/gzjj/202002/t20200207_6274584.htm">
+                                        居家隔离，体温监测
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://gdgz.wenming.cn/gzjj/202002/t20200207_6274584.htm">
+                                        专户专人，隔离值守
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -462,21 +480,23 @@
                       Add '.widget-29182-v2' class for v2 style
                     -->
                     <div class="widget-29182 widget-29182-v2 mb-0 pb-0">
+                        <div class="widget-inner">
+                            <h3>社区出入管理</h3>
+                        </div>
                         <div class="widget-inner"><img src="assets/frontend/images/img_sm_4.jpg" alt="Image"
                                                        class="img-mb-2 img-fluid">
                         </div>
                         <div class="widget-inner">
-                            <h3>Home Quarantine Guidelines</h3>
-                        </div>
-
-                        <div class="widget-inner">
                             <ul class="list-unstyled">
-                                <li><a href="#">Executive Order from the Mayor</a></li>
-                                <li><a href="#">Business &amp; Industry Advice</a></li>
-                                <li><a href="#">Home Quarantine</a></li>
-                                <li><a href="#">Community Food Supply</a></li>
-                                <li><a href="#">Community Lockdown</a></li>
-                                <li><a href="#">Skeletal Work Force Arrangement</a></li>
+                                <li>
+                                    <a href="https://baijiahao.baidu.com/s?id=1658172044602323644&wfr=spider&for=pc">发放出入证，精准识别</a>
+                                </li>
+                                <li>
+                                    <a href="https://baijiahao.baidu.com/s?id=1658172044602323644&wfr=spider&for=pc">设立检查点，体温监测</a>
+                                </li>
+                                <li>
+                                    <a href="https://baijiahao.baidu.com/s?id=1658172044602323644&wfr=spider&for=pc">封闭式管理，外来人员禁入</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -487,5 +507,6 @@
         </div>
     </div>
     <!-- END: .site-section -->
+
 </div>
 <!-- END: #main -->
